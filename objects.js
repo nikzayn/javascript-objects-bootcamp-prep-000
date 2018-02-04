7e24b422b9b0e4a15c89376cba468ee341c46878
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-04 18:14:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,5 +8,6 @@ function updatePlaylist(obj, key, value){
   return Object.assign({}, obj, {[key]: value})
 }
 function removeFromPlaylist(playlist, bruno){
-  delete playlist.bruno;
+  delete playlist[bruno];
+  return pla
 }
